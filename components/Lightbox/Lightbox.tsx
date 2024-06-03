4f628bebdc43e0c5fd4512359c10848341596f35
Matheus Murden <matheus@murden.dev>
2024-06-03 11:46:48 -0300

    chore: enhance back navigation of Lightbox component

--- components/Lightbox/Lightbox.tsx
+++ components/Lightbox/Lightbox.tsx
@@ -3,7 +3,7 @@
 import {useSearchParams, useRouter} from "next/navigation";
 
 import styles from './Lightbox.module.css';
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 import Player from '@vimeo/player';
 import { track } from '@vercel/analytics';
@@ -42,16 +42,28 @@ export const Lightbox = () => {
 					}
 				});
 			}
-			track('Watched Video', { video: `https://vimeo.com/${vimeoId}`});
+			iframePlayer.getVideoTitle().then((title) => {
+				track('Watched Video', { video: title, url: `https://vimeo.com/${vimeoId}`});
+			})
+			
 		}
 	}, [modal, vimeoId])
 
 	const iframeRef = useRef<HTMLIFrameElement>(null)
 	const loadingRef = useRef<HTMLDivElement>(null)
+
+
+	const navigateBack = useCallback(() => {
+		if (window.history?.length && window.history.length > 1) {
+			router.back();
+		 } else {
+			router.replace(window.location.pathname);
+		 }
+	}, [router])
 	
 	return modal && (
-		<dialog onClick={() => router.back()} className="animate-fade-in-up fixed z-[991] min-h-screen top-0 left-0 bg-[#00000095] flex flex-row items-center p-0 md:px-10 md:py-0 w-full">
-			<div onClick={() => router.back()} className={`${aspectClass} ${styles.lightbox} shadow-2xl rounded overflow-hidden border-none relative max-h-screen h-full overflow-hidden w-full`}>
+		<dialog onClick={() => navigateBack()} className="animate-fade-in-up fixed z-[991] min-h-screen top-0 left-0 bg-[#00000095] flex flex-row items-center p-0 md:px-10 md:py-0 w-full">
+			<div onClick={() => navigateBack()} className={`${aspectClass} ${styles.lightbox} shadow-2xl rounded overflow-hidden border-none relative max-h-screen h-full overflow-hidden w-full`}>
 				<div ref={loadingRef} className="bg-black absolute top-0 left-0 w-full h-full z-[999] flex items-center justify-center">
 				<div
 					className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
